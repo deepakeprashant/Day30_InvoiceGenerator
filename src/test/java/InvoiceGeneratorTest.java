bf07c8ad992fc4859837f1e1e07c0c814cb4dcea
Prashant Deepake <deepakeprashant1@gmail.com>
2023-02-26 22:22:36 +0530

    Added Calculate Fare and Minimum Fare

---
+++ src/test/java/InvoiceGeneratorTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class InvoiceGeneratorTest {
+
+    InvoiceGenerator invoiceGenerator;
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnFare() {
+        invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculatorFare(distance, time);
+        Assertions.assertEquals(25.0, fare);
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinimumFare() {
+        invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculatorFare(distance, time);
+        Assertions.assertEquals(5.0, fare);
+    }
+}
